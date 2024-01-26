fc789e8b65d1c782170e2f945a914418cb86606b
Gabriel Loureiro <gabrieloureirof@gmail.com>
2024-01-26 08:09:08 -0300

    feat: add boilerplate v.1.1

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import * as Components from './components'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Components.NoMatch />
+      {/* Your Job here */}
+    </BrowserRouter>
+  </React.StrictMode>,
+)
